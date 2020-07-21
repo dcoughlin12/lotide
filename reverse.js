e45e1fa25d9467ee9486411d2c670e0ab5e023a6
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-21 01:55:12 +0000

    completed string reversal

---
+++ reverse.js
@@ -0,0 +1,17 @@
+
+let string = process.argv;
+string = string.slice(2);
+
+const reverse = function(string) {
+  for (let x = 0; x < string.length; x++) {
+    console.log(wordReversal(string[x]));
+  }
+};
+const wordReversal = function(string) {
+  let backwards = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    backwards += string[i];
+  } return backwards;
+};
+reverse(string);
+

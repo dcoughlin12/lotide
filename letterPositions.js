e0307da56ee198f4ca38cba694df4a25e36482fd
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-22 23:19:30 +0000

    completed Letter Position Function

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let eachLetter = sentence[i];
+    //console.log(eachLetter)
+    if (!results[eachLetter]) {
+      results[eachLetter] = [];
+    }
+    results[eachLetter].push(i);
+  }
+  delete results[' '];
+  return results;
+};
+
+console.log(letterPositions('lighthouse in the house'));

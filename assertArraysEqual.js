002dd13015f7b0bcbf06dc39f531c5f9ca4b41d2
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-27 18:19:08 +0000

    refactored with function callbacks and seperate test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,12 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let TrueOrFalse = '';
-  if (arrayOne.length < 1 && arrayTwo.length < 1){
-    return true
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    let index = i;
-    if (arrayOne[i] === arrayTwo[index]) {
-      TrueOrFalse = true;
-    } else {
-      return false;
-    }
-  } return TrueOrFalse;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArr, secondArr) {
   if (firstArr.length === secondArr.length && eqArrays(firstArr, secondArr)) {
-    console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+    return `✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`;
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+    return `❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`;
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual
 
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-
-
-//!!!!!!!!!!!!!!!!!!   this code fails if we have one array longer than the other.. still returns true.!!!!!!!!!!
